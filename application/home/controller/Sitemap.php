f870e0b3f29f012013b04788eaad7a89fe61cba2
丁敬 <dingjing@live.cn>
2019-05-05 13:19:36 +0800

    sitemap

--- application/home/controller/Sitemap.php
+++ application/home/controller/Sitemap.php
@@ -1,23 +1,36 @@
 <?php
 
 namespace app\home\controller;
-
-use app\common\controller\Base;
+use think\Controller;
 
 /**
  *  首页
  *
  *
  */
-class Sitemap extends Base {
+class Sitemap extends Controller {
     public function index() {
+        $buf = '';
         $info=model('common/api')->get_novel_sitemap();
         if(!$info){
             $error = model('common/api')->getError();
             $this->error(empty($error) ? '没有找到 sitemap' : $error,url('Home/Index/index'));
         }
 
-        $this->assign('sitemap',$info);
-        return $this->fetch($this->home_tplpath.'sitemap.xml');
+        $buf = '<?xml version="1.0" encoding="UTF-8"?>' .
+                    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' .
+                        '<url>' .
+                            '<loc>https://enjoyread.top</loc>' .
+                            '<lastmod>2019-05-01</lastmod>' .
+                            '<changefreq>weekly</changefreq>' .
+                            '<priority>1</priority>' .
+                        '</url>';
+        foreach ($info as $ik=>$iv){
+            $buf .= '<url><loc>' . 'https://enjoyread.top/home/novel/index/id/' . $iv['id'] . '.html</loc>';
+            $buf .= '<lastmod>' . date('Y-m-d h:i:s',$iv['update_time']) . '</lastmod>';
+            $buf .= '<changefreq>weekly</changefreq><priority>1</priority></url>';
+        }
+        $buf .= '</urlset>';
+        return $result =  xml($buf,200,[],['root_node'=>'xml']);
     }
 }
