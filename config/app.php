6b23d62ce996a4298d6987578b91c06a43f4fa46
丁敬 <dingjing@live.cn>
2019-05-05 11:02:08 +0800

    update

--- config/app.php
+++ config/app.php
@@ -15,11 +15,11 @@
 
 return [
     // 应用名称
-    'app_name'               => '',
+    'app_name'               => 'enjoyread',
     // 应用地址
     'app_host'               => '',
     // 应用调试模式
-    'app_debug'              => true,
+    'app_debug'              => false,
     // 应用Trace
     'app_trace'              => false,
     // 是否支持多模块
